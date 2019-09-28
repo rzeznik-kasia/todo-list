c88172a06bac4ec8c5595e54f07e730c93c57aa8
Katarzyna Rzeznik <rzeznik.kasia@gmail.com>
2019-09-28 15:18:17 +0200

    Uses html template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-  <h1>
-    Welcome to {{ title }}!
-  </h1>
-  <app-input-button-unit></app-input-button-unit>
-`,
+ templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'todo-list';
 }
-// templateUrl: './app.component.html',
+
