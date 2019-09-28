e1ba399bc753c1555ba4126a6d72bb2bae0f0688
Katarzyna Rzeznik <rzeznik.kasia@gmail.com>
2019-09-28 15:15:55 +0200

    Inline template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+  <h1>
+    Welcome to {{ title }}!
+  </h1>
+`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
