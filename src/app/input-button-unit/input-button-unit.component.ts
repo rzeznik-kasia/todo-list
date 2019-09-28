9030311016bfef2c689c63654d6d8ace21ceb0aa
Katarzyna Rzeznik <rzeznik.kasia@gmail.com>
2019-09-28 15:16:26 +0200

    Creating new component

---
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-input-button-unit',
+  templateUrl: './input-button-unit.component.html',
+  styleUrls: ['./input-button-unit.component.css']
+})
+export class InputButtonUnitComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
