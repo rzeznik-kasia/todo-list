75dbf033855a2c9807273d7748d8be8f2d5b4aa3
Katarzyna Rzeznik <rzeznik.kasia@gmail.com>
2019-09-28 15:17:03 +0200

    Uses new component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,11 @@ import { Component } from '@angular/core';
   <h1>
     Welcome to {{ title }}!
   </h1>
+  <app-input-button-unit></app-input-button-unit>
 `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'todo-list';
 }
+// templateUrl: './app.component.html',
